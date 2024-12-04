74dbc7f250013329f4b2e9b582000b44aaf964a1
Stephane Senart <>
2024-12-04 15:19:41 +0100

    Add entrypoint.sh script to be invoked in the container

---
+++ entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Check/Set default values to optional environment variables
+: "${GRDF_USERNAME:?GRDF_USERNAME is required and not set.}"
+: "${GRDF_PASSWORD:?GRDF_PASSWORD is required and not set.}"
+: "${GRDF_PCE_IDENTIFIER:?GRDF_PCE_IDENTIFIER is required and not set.}"
+: ${GRDF_SCAN_INTERVAL:="480"} # 8 hours
+: ${GRDF_LAST_DAYS:="1095"} # 3 years
+
+: "${MQTT_BROKER:?MQTT_BROKER is required and not set.}"
+: ${MQTT_PORT:="1883"} # Default MQTT port
+: ${MQTT_USER:=""} # No user
+: ${MQTT_PASSWORD:=""} # No password
+
+# Display environment variables
+echo "GRDF_USERNAME: ${GRDF_USERNAME}"
+echo "GRDF_PASSWORD: ***************"
+echo "GRDF_PCE_IDENTIFIER: ${GRDF_PCE_IDENTIFIER}"
+echo "GRDF_SCAN_INTERVAL: ${GRDF_SCAN_INTERVAL}"
+echo "GRDF_LAST_DAYS: ${GRDF_LAST_DAYS}"
+echo "MQTT_BROKER: ${MQTT_BROKER}"
+echo "MQTT_PORT: ${MQTT_PORT}"
+echo "MQTT_USER: ${MQTT_USER}"
+echo "MQTT_PASSWORD: ***************"
+
+# Instantiate the template config
+envsubst < "/app/config/configuration.template.yaml" > "/app/config/configuration.yaml"
+envsubst < "/app/config/secrets.template.yaml" > "/app/config/secrets.yaml"
+
+# Run the gazpar2mqtt python program
+cd /app/config
+python3 -m gazpar2mqtt
