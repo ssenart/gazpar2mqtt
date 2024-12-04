7e55d0efb7d73d367d8fe57c80a807942a2ba52f
Stephane Senart <>
2024-12-04 17:45:13 +0100

    In entrypoint.sh, add double quotes around default variables.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@
 : "${GRDF_USERNAME:?GRDF_USERNAME is required and not set.}"
 : "${GRDF_PASSWORD:?GRDF_PASSWORD is required and not set.}"
 : "${GRDF_PCE_IDENTIFIER:?GRDF_PCE_IDENTIFIER is required and not set.}"
-: ${GRDF_SCAN_INTERVAL:="480"} # 8 hours
-: ${GRDF_LAST_DAYS:="1095"} # 3 years
+: "${GRDF_SCAN_INTERVAL:="480"}" # 8 hours
+: "${GRDF_LAST_DAYS:="1095"}" # 3 years
 
 : "${MQTT_BROKER:?MQTT_BROKER is required and not set.}"
-: ${MQTT_PORT:="1883"} # Default MQTT port
-: ${MQTT_USER:=""} # No user
-: ${MQTT_PASSWORD:=""} # No password
+: "${MQTT_PORT:="1883"}" # Default MQTT port
+: "${MQTT_USERNAME:=""}" # No user
+: "${MQTT_PASSWORD:=""}" # No password
 
 # Display environment variables
 echo "GRDF_USERNAME: ${GRDF_USERNAME}"
@@ -20,7 +20,7 @@ echo "GRDF_SCAN_INTERVAL: ${GRDF_SCAN_INTERVAL}"
 echo "GRDF_LAST_DAYS: ${GRDF_LAST_DAYS}"
 echo "MQTT_BROKER: ${MQTT_BROKER}"
 echo "MQTT_PORT: ${MQTT_PORT}"
-echo "MQTT_USER: ${MQTT_USER}"
+echo "MQTT_USERNAME: ${MQTT_USERNAME}"
 echo "MQTT_PASSWORD: ***************"
 
 # Instantiate the template config
