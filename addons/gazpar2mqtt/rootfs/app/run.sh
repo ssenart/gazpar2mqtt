bbfa04c1c9f399d7c4c453338d70a5f7a75d43b8
Stéphane Senart <>
2025-01-25 22:24:00 +0100

    [#2] Deploy gazpar2mqtt as an HA add-on

--- addons/gazpar2mqtt/rootfs/app/run.sh
+++ addons/gazpar2mqtt/rootfs/app/run.sh
@@ -1,54 +1,29 @@
 #!/usr/bin/with-contenv bashio
 
-GRDF_USERNAME=$(bashio::config 'grdf.username')
-GRDF_PASSWORD=$(bashio::config 'grdf.password')
-GRDF_PCE_IDENTIFIER=$(bashio::config 'grdf.pce_identifier')
-GRDF_SCAN_INTERVAL=$(bashio::config 'grdf.scan_interval')
-GRDF_LAST_DAYS=$(bashio::config 'grdf.last_days')
-GRDF_TIMEZONE=$(bashio::config 'grdf.timezone')
+# Load the Add-on configuration in JSON and reformat it to YAML.
+GRDF_JSON="'grdf': { 'scan_interval': $(bashio::addon.config 'scan_interval'), 'devices': $(bashio::addon.config 'devices') } }"""
+GRDF_CONFIG=$(echo $GRDF_JSON | yq -P)
 
-HOMEASSISTANT_HOST=supervisor
-HOMEASSISTANT_PORT=80
-HOMEASSISTANT_TOKEN=${SUPERVISOR_TOKEN}
-
-# Check/Set default values to optional environment variables
-: "${GRDF_USERNAME:?GRDF_USERNAME is required and not set.}"
-: "${GRDF_PASSWORD:?GRDF_PASSWORD is required and not set.}"
-: "${GRDF_PCE_IDENTIFIER:?GRDF_PCE_IDENTIFIER is required and not set.}"
-: "${GRDF_SCAN_INTERVAL:="480"}" # 8 hours
-: "${GRDF_LAST_DAYS:="365"}" # 1 year
-: "${GRDF_TIMEZONE:="Europe/Paris"}" # Default timezone: Europe/Paris
-
-: "${HOMEASSISTANT_HOST:?HOMEASSISTANT_HOST is required and not set.}"
-: "${HOMEASSISTANT_PORT:="8123"}" # Default Home Assistant port
-: "${HOMEASSISTANT_ENDPOINT:="/core/websocket"}" # Default Home Assistant endpoint
-: "${HOMEASSISTANT_TOKEN:?HOMEASSISTANT_TOKEN is required and not set.}"
+MQTT_JSON="{ 'mqtt': $(bashio::config 'mqtt') }"
+MQTT_CONFIG=$(echo $MQTT_JSON | yq -P)
 
 # Display environment variables
-echo "GRDF_USERNAME: ${GRDF_USERNAME}"
-echo "GRDF_PASSWORD: ***************"
-echo "GRDF_PCE_IDENTIFIER: ${GRDF_PCE_IDENTIFIER}"
-echo "GRDF_SCAN_INTERVAL: ${GRDF_SCAN_INTERVAL}"
-echo "GRDF_LAST_DAYS: ${GRDF_LAST_DAYS}"
-echo "GRDF_TIMEZONE: ${GRDF_TIMEZONE}"
-echo "HOMEASSISTANT_HOST: ${HOMEASSISTANT_HOST}"
-echo "HOMEASSISTANT_PORT: ${HOMEASSISTANT_PORT}"
-echo "HOMEASSISTANT_ENDPOINT: ${HOMEASSISTANT_ENDPOINT}"
-echo "HOMEASSISTANT_TOKEN: ${HOMEASSISTANT_TOKEN}"
+bashio::log.info "GRDF_CONFIG: ${GRDF_CONFIG}"
+bashio::log.info "MQTT_CONFIG: ${MQTT_CONFIG}"
 
 # Export environment variables
-export GRDF_USERNAME GRDF_PASSWORD GRDF_PCE_IDENTIFIER GRDF_SCAN_INTERVAL GRDF_LAST_DAYS GRDF_TIMEZONE HOMEASSISTANT_HOST HOMEASSISTANT_PORT HOMEASSISTANT_ENDPOINT HOMEASSISTANT_TOKEN
+export GRDF_CONFIG MQTT_CONFIG
 
 # Instantiate the template config
 if [ ! -e /app/config/configuration.yaml ]; then
     envsubst < "/app/config/configuration.template.yaml" > "/app/config/configuration.yaml"
 fi
 
-# Instantiate the template secrets
-if [ ! -e /app/config/secrets.yaml ]; then
-    envsubst < "/app/config/secrets.template.yaml" > "/app/config/secrets.yaml"
-fi
+# Display the configuration file: Uncomment below for debugging
+# echo "Configuration file:"
+# cat /app/config/configuration.yaml
 
-# Run the gazpar2haws python program
+# Run the gazpar2mqtt python program
 cd /app
-python3 -m gazpar2mqtt
+
+python3 -m gazpar2mqtt --config config/configuration.yaml --secrets config/secrets.yaml
